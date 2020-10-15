4dc1055b5af2349b683626672eeaf6655e1bca63
Sam Caldwell <mail@samcaldwell.net>
2020-10-15 12:34:03 -0500

    WIP: refactor source sequence generator using common/counter.  Needs tests, but it helped to review counter functionality and ensure the approach is proper.

--- tools/common/source/generatesequence.go
+++ tools/common/source/generatesequence.go
@@ -1,7 +1,7 @@
 package source
 
 import (
-	"fmt"
+	"asymmetric-effort/asymmetric-toolkit/tools/common/counter"
 )
 
 func (o *Source) generateSequence() {
@@ -9,50 +9,19 @@ func (o *Source) generateSequence() {
 		Generate words as a sequence within a given keyspace (allowedChars).
 		For example: A B C AA AB AC BA BB BC CA CB CC and so on where the
 		length of the words is determined by config.WordSize.
-
-		allowedRunes translates the allowedChars string into an array of bytes (runes)
-		The position of the rune in this array is the value in the counter.
 	 */
-	allowedRunes :=
-	runeCount:= uint8(len(allowedRunes))
-
-	genString := func(counter []uint8) string {
-		var runeString []rune = make([]rune, len(counter))
-		for i := uint8(0); i < uint8(len(counter)); i++ {
-			runeString[i] = allowedRunes[counter[i]]
-		}
-		s := string(runeString)
-		return s
-	}
-
 	checkWordSize := func(wsz int) bool {
 		return wsz <= int(o.config.WordSize)
 	}
 	checkWordCount := func() bool {
 		return o.counter <= int(o.config.MaxWordCount)
 	}
-	checkWordLength := func(word *string) bool {
-		return len(*word) != 0
-	}
 	for wordSize := 1; checkWordSize(wordSize) && checkWordCount(); wordSize++ {
-		counter := make([]uint8, wordSize)
-		//Generator factory function
-		np := func() string {
-			//then we create a counter variable which will roll through like an odometer.
-			increment(&counter,0, runeCount)
-			s:= genString(counter)
-			fmt.Printf("wordSize:     %d\n", wordSize)
-			fmt.Printf("allowedRunes: %v\n", allowedRunes)
-			fmt.Printf("counter:      %v\n", counter)
-			fmt.Printf("result:       '%s'\n", s)
-			return s
-		} //Execute generator factory function
-
-		for word := np(); checkWordLength(&word) && checkWordCount(); word = np() {
-			o.WaitIfPaused()
-			o.feed <- word
+		var generator counter.Counter
+		generator.Setup(*o.allowedChars, wordSize)
+		for generator.Increment(0) && checkWordCount() { //Iterate through the field of wordSize chars.
+			o.feed <- generator.String() //Produce a new word for each one and push to the feed.
 			o.counter++
 		}
-
 	}
 }
