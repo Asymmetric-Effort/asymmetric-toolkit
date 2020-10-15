5d3ae43226a287c635138b9fcfc620d7d1bbce03
Sam Caldwell <mail@samcaldwell.net>
2020-10-15 12:32:58 -0500

    WIP: Bug fix for Counter::increment()

--- tools/common/counter/increment.go
+++ tools/common/counter/increment.go
@@ -1,18 +1,18 @@
 package counter
 
-func (o *Counter) Increment(c *[]uint8,p int) bool {
+func (o *Counter) Increment(p int) bool {
 	/*
 		Recursively increment the counter from lsb(0) to msb(n)
 		and return true if we still have room to increment or
 		false if we have reached the counter's maximum value.
 	 */
-	if (*c)[p]<o.maxBase {
-		(*c)[p]++
+	if (*o.data)[p]<o.maxBase {
+		(*o.data)[p]++
 		return true //yes, we incremented. You can keep going.
 	}else{
 		if p<len(*o.data) {
-			(*c)[p] = 0
-			return o.Increment(c, p+1)
+			(*o.data)[p] = 0
+			return o.Increment(p+1)
 		}else{
 			return false //no, we're done.
 		}
