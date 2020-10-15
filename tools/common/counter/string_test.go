d6e04076e76d6f03ea62f137812ac62fbb5ac278
Sam Caldwell <mail@samcaldwell.net>
2020-10-15 14:40:37 -0500

    Add string_test for common/counter.  Feature complete.

---
+++ tools/common/counter/string_test.go
@@ -0,0 +1,19 @@
+package counter
+
+import (
+	"asymmetric-effort/asymmetric-toolkit/tools/common/errors"
+	"testing"
+)
+
+func TestCounter_String(t *testing.T) {
+	var c Counter
+	const charset="0123456789"
+	const wordSize=2
+	c.runes = func() *[]rune { d := []rune(charset); return &d }()
+	c.maxBase = uint8(len(*c.runes)-1)
+	c.data = func() *[]uint8 { d := make([]uint8, wordSize); return &d }()
+	errors.Assert(c.String()=="00", "Expect 00 in initial state.")
+	(*c.data)[0]=1
+	(*c.data)[1]=2
+	errors.Assert(c.String()=="12", "Expect 12 in initial state.")
+}
