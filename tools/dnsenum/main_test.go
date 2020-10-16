7a4af1983e9b7646498ffd2a2ca92b4aedb5d7cf
Sam Caldwell <mail@samcaldwell.net>
2020-10-15 19:58:39 -0500

    Add a tombstone timeout for all tests.  This keeps CI/CD from spinning endlessly on hung tests.

--- tools/dnsenum/main_test.go
+++ tools/dnsenum/main_test.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 func TestDnsEnum(t *testing.T){
-
+	go func(){
+		<-time.After(time.Minute * 15)
+		t.Fatal("Main() Test: Terminating after timeout (15min)")
+	}()
 }
