4bece4deeaec0fb53578f6aac048cc322f6422ac
Sam Caldwell <mail@samcaldwell.net>
2020-10-16 11:22:15 -0500

    Add a simple binary test (charset='01') for sequence generator

--- tools/common/source/generatesequence_test.go
+++ tools/common/source/generatesequence_test.go
@@ -50,3 +50,46 @@ func TestSourceGenerateSequence(t *testing.T) {
 	}
 	fmt.Println("TestSourceGenerateSequence...Done")
 }
+
+func TestSourceGenerateSequenceBinary(t *testing.T) {
+	/*
+		Setup
+	*/
+	const keyspace = "01"
+	const maxWordSize = 9
+	var s Source
+	var config cli.Configuration
+	args := []string{"--domain", "google.com", "--mode", "sequence"}
+	config.Parse(args)
+	s.config = &config
+	go func() {
+		<-time.After(time.Second * 500)
+		t.Fatal("Terminating after timeout (500s)")
+	}()
+
+	for s.config.WordSize = 1; s.config.WordSize < maxWordSize; s.config.WordSize++ {
+		s.config.MaxWordCount = 1000000000
+		s.allowedChars = func() *string { str := keyspace; return &str }()
+		s.feed.Setup(cli.SourceBufferSz * 1000)
+		/*
+			Run Generator
+		*/
+		fmt.Printf("Starting generator.  Size:%d of %d\n", s.config.WordSize, maxWordSize)
+		s.generateSequence()
+		s.feed.Close()
+		fmt.Printf("Finished generating.  Number items in channel: %d\n", s.feed.Length())
+		/*
+			Analyze Result
+		*/
+		expectedCount := s.feed.Length()
+		last := ""
+		for i := 0; i <= expectedCount; i++ {
+			if s.feed.Length() > 0 {
+				last = s.feed.Pop()
+			}
+		}
+		errors.Assert(s.feed.Length() == 0, "Expected to have consumed all elements")
+		fmt.Printf("Consumed queue of %d elements (last:%s)\n", expectedCount, last)
+	}
+	fmt.Println("TestSourceGenerateSequenceBinary...Done")
+}
