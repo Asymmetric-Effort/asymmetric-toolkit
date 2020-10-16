27659aea55aadb4cbe776410a5bce34d3675cca4
Sam Caldwell <mail@samcaldwell.net>
2020-10-16 11:20:49 -0500

    Output fixes for sequence generator tests.

--- tools/common/source/generatesequence_test.go
+++ tools/common/source/generatesequence_test.go
@@ -5,6 +5,7 @@ import (
 	"asymmetric-effort/asymmetric-toolkit/tools/dnsenum/cli"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestSourceGenerateSequence(t *testing.T) {
@@ -12,20 +13,25 @@ func TestSourceGenerateSequence(t *testing.T) {
 		Setup
 	*/
 	const keyspace = "0123456789"
+	const maxWordSize = 9
 	var s Source
 	var config cli.Configuration
 	args := []string{"--domain", "google.com", "--mode", "sequence"}
 	config.Parse(args)
 	s.config = &config
-	for s.config.WordSize = 1; s.config.WordSize < 9; s.config.WordSize++ {
+	go func() {
+		<-time.After(time.Second * 500)
+		t.Fatal("Terminating after timeout (500s)")
+	}()
 
+	for s.config.WordSize = 1; s.config.WordSize < maxWordSize; s.config.WordSize++ {
 		s.config.MaxWordCount = 1000000000
 		s.allowedChars = func() *string { str := keyspace; return &str }()
 		s.feed.Setup(cli.SourceBufferSz * 1000)
 		/*
 			Run Generator
 		*/
-		fmt.Printf("Starting generator.  Size:%d\n", s.config.WordSize)
+		fmt.Printf("Starting generator.  Size:%d of %d\n", s.config.WordSize, maxWordSize)
 		s.generateSequence()
 		s.feed.Close()
 		fmt.Printf("Finished generating.  Number items in channel: %d\n", s.feed.Length())
@@ -40,6 +46,7 @@ func TestSourceGenerateSequence(t *testing.T) {
 			}
 		}
 		errors.Assert(s.feed.Length() == 0, "Expected to have consumed all elements")
-		fmt.Printf("Consumed queue of %d elements (last:%s)", expectedCount, last)
+		fmt.Printf("Consumed queue of %d elements (last:%s)\n", expectedCount, last)
 	}
+	fmt.Println("TestSourceGenerateSequence...Done")
 }
