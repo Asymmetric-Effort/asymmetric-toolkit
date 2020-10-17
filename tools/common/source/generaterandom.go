0c2218544ac1e31e5a30d26c189dd334a0830546
Sam Caldwell <mail@samcaldwell.net>
2020-10-16 23:16:52 -0500

    retrofit Source::generateRandom() to use random.String() rather than one-off code.  Tests pass.

--- tools/common/source/generaterandom.go
+++ tools/common/source/generaterandom.go
@@ -1,21 +1,12 @@
 package source
 
 import (
-	"math/rand"
-	"time"
+	"asymmetric-effort/asymmetric-toolkit/tools/common/random"
 )
 
 func (o *Source) generateRandom() {
-	keySpace := []rune(*o.allowedChars)
-	numRunes := len(*o.allowedChars)
-	word := make([]rune, o.config.WordSize.Get())
-
 	for o.counter = 1; o.counter <= int(o.config.MaxWordCount); o.counter++ {
 		o.WaitIfPaused()
-		seededRand := rand.New(rand.NewSource(time.Now().UnixNano() + int64(o.counter)))
-		for i := range word {
-			word[i] = keySpace[seededRand.Intn(numRunes)]
-		}
-		o.feed.Push(string(word)) //ToDo: Add FiFO queue with blocking send
+		o.feed.Push(random.String(o.config.WordSize.Get(),o.allowedChars))
 	}
 }
