48ae0f521dc6920f752815558e7580bd01340332
Sam Caldwell <github@samcaldwell.net>
2020-10-19 23:47:34 -0500

    Fixes for HighEntropy after fixes to GetShannons() were submitted earlier.  Tests pass.

--- tools/common/entropy/HighEntropy_test.go
+++ tools/common/entropy/HighEntropy_test.go
@@ -2,15 +2,59 @@ package entropy
 
 import (
 	"asymmetric-effort/asymmetric-toolkit/tools/common/errors"
-	"github.com/google/uuid"
+	"fmt"
 	"testing"
 )
 
 func TestHighEntropy(t *testing.T) {
-	errors.Assert(!HighEntropy("1111111111111111111111"), "Expected false")
-	errors.Assert(!HighEntropy("0"), "Expected false")
-	errors.Assert(!HighEntropy("false"), "Expected false")
-	errors.Assert(!HighEntropy("668108162888"), "Expected false")
-	errors.Assert(HighEntropy(func()string{u,_:=uuid.NewUUID();return u.String()}()), "Expected true")
-	errors.Assert(HighEntropy("ZmYwOTZmNmQyNWFjMWY4ZGY4MDBjNjQ3N2IwOGMxMDY4NTE1ODFjMjhlZmRjZGNmZmE2ZTM2MTQ4NjA2YTFkNDM2MDljZjc1MDFhODgxOTI0NGZmMmNmNmE1NWEyNDEzNmJjMWQxZmVkMmUwZmQ4ZDc5ODdiMjhiNzU4ZWUzYWYK"), "Expected true")
+	var tests = []struct {
+		input string
+		result bool
+	}{
+		{
+			"1111111111111111111111",
+			false,
+		}, {
+			"",
+			false,
+		}, {
+			"0",
+			false,
+		}, {
+			"false",
+			true,
+		}, {
+			"668108162888",
+			true,
+		}, {
+			"1234567890",
+			true,
+		}, {
+			"12345678901234567890",
+			true,
+		}, {
+			"123456789012345678901234567890",
+			true,
+		}, {
+			"12345678901234567890123456789012345678901234567890",
+			true,
+		},
+	}
+	for i, test := range tests {
+		fmt.Printf("Test (%d) Result:%d (%t)\n", i, GetShannons(test.input),HighEntropy(test.input))
+		if test.result {
+			errors.Assert(HighEntropy(test.input),
+				fmt.Sprintf("Expected %t (%d) (%d)", HighEntropy(test.input), i, GetShannons(test.input)))
+		} else {
+			errors.Assert(!HighEntropy(test.input),
+				fmt.Sprintf("Expected %t (%d) (%d)", HighEntropy(test.input), i, GetShannons(test.input)))
+		}
+
+
+	}
 }
+
+/*
+	1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 => 4.5849 shannons
+	1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6 1 2 3 4 5 6 => 4.3983 shannons
+ */
