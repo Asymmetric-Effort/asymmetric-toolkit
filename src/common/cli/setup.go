ee53ee52a9b1f30ccc1183b39794cbb61d94d74f
Sam Caldwell <github@samcaldwell.net>
2020-10-22 21:59:29 -0500

    WIP: continuing to refactor commandline interface for reusability.  CLI pieces can now be abstracted into the various packages related to them and integrated in the CLI/Commandline::Setup() method

--- src/common/cli/setup.go
+++ src/common/cli/setup.go
@@ -14,12 +14,17 @@ func (o *Configuration) Setup(spec *Specification) (err error) {
 	var count int         //count the flags for later comparison.
 	var next = ExpectFlag // We always expect the first arg to be a --flag.
 
+	/*
+		This is where we aggregate (left join) our commandline specifications
+		into a single final view.  Each subsequent layer can override the previous.
+	*/
 	o.spec = CommandLineSpecification(o)
-	MergeMaps(o.spec, logger.CommandLineSpecification(o)) // Get the logger cli specification.
-	MergeMaps(o.spec, source.CommandLineSpecification(o)) // Adding source CLI.
-	MergeMaps(o.spec, spec)                               // This is the design of our cli expectation.
+	MergeMaps(o.spec, logger.CommandLineSpecification(o))
+	MergeMaps(o.spec, source.CommandLineSpecification(o))
+	MergeMaps(o.spec, spec)
 
-	o.args = make(Arguments) // Initialize the arguments object for use before parsing.
+	// Initialize the arguments object for use before parsing.
+	o.args = make(Arguments)
 	/*
 		Parse the arguments
 	*/
