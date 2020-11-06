33c8f0c39fe6c42c01af8703229fb0c0e8270775
Sam Caldwell <github@samcaldwell.net>
2020-11-06 13:54:04 -0600

    WIP: continuing to add tests for common/cli and fixing bugs as they are uncovered.

--- src/common/cli/parserInt_test.go
+++ src/common/cli/parserInt_test.go
@@ -1,2 +1,83 @@
 package cli
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"testing"
+)
+
+func TestParserInt(t *testing.T) {
+	go func() { //Lower and Upper bound.
+		fmt.Println("upper/lower bound test started")
+		min := 1
+		max := 5
+		parser := ParserInt(min, max)
+
+		testRun := func(v string, p bool) {
+			if !p {
+				fmt.Printf("Expect failure for %v\n", v)
+				defer func() { recover() }()
+			}
+			err, val := parser(&v)
+			if err != nil {
+				panic(err)
+			}
+			if val.Value != v {
+				t.Errorf("Value mismatch: %s", v)
+			}
+		}
+		for i := min - 10; i <= max*2; i++ {
+			strValue := strconv.Itoa(i)
+			testRun(strValue, i >= min && i <= max)
+		}
+		fmt.Println("Lower/Upper bound test passes")
+	}()
+	go func() { //No upper bound
+		fmt.Println("no-upper-bound test started")
+		parser := ParserInt(1)
+
+		testRun := func(v string, p bool) {
+			if !p {
+				fmt.Printf("Expect failure for %v\n", v)
+				defer func() { recover() }()
+			}
+			err, val := parser(&v)
+			if err != nil {
+				panic(err)
+			}
+			if val.Value != v {
+				t.Errorf("Value mismatch: %s", v)
+			}
+		}
+		testRun("0", false)
+		testRun("1", true)
+		for i := 1; i < math.MaxInt32; i+=2 {
+			testRun(strconv.Itoa(i), true)
+		}
+		fmt.Println("No-Upper-bound test passes")
+	}()
+	func() { //no lower or upper bound
+		fmt.Println("unbounded test started")
+		parser := ParserInt()
+
+		testRun := func(v string, p bool) {
+			if !p {
+				fmt.Printf("Expect failure for %v\n", v)
+				defer func() { recover() }()
+			}
+			err, val := parser(&v)
+			if err != nil {
+				panic(err)
+			}
+			if val.Value != v {
+				t.Errorf("Value mismatch: %s", v)
+			}
+		}
+		for i := math.MinInt32; i < math.MaxInt32; i+=10 {
+			testRun(strconv.Itoa(i), true)
+		}
+		fmt.Println("unbounded test passes")
+	}()
+
+}
