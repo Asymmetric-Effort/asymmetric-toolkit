9ce4bb034dfc52ec440df536b3a250bc6b71cb27
Sam Caldwell <github@samcaldwell.net>
2020-11-08 15:02:24 -0600

    Add list parser to common/cli

--- src/common/cli/parseList.go
+++ src/common/cli/parseList.go
@@ -1,2 +1,29 @@
 package cli
 
+import (
+	"strings"
+)
+
+/*
+	ParseList() is a parser function factory which returns a parser function to the caller.
+	This parser function will evaluate a given commandline argument (arg *string) and perform
+	any validation before returning an appropriate error and Argument object pointer.
+*/
+
+func ParseList(delimiter string) (parser func(arg *string) (err error, val *Argument)) {
+	/*
+		Create the parser function and return it at the end.
+	*/
+	parser = func(arg *string) (err error, val *Argument) {
+		//
+		//	Start off by converting our string commandline argument to its numeric equivalent.
+		//
+		_ = strings.Split(*arg, delimiter)
+		return nil, &Argument{
+			List,
+			*arg,
+		}
+	}
+	//return the parser function to the Specification.
+	return parser
+}
--- src/common/cli/parseList_test.go
+++ src/common/cli/parseList_test.go
@@ -1,2 +1,45 @@
 package cli
 
+import (
+	"testing"
+)
+
+func TestParseList(t *testing.T) {
+	func() {
+		testRun := func(v string, delimiter string) {
+
+			parser := ParseList(delimiter)
+
+			err, val := parser(&v)
+
+			if err != nil {
+				panic(err)
+			}
+
+			if val.Value != v {
+				t.Errorf("Value mismatch: %s", v)
+			}
+
+		}
+
+		tests := []struct {
+			Value     string
+			delimiter string
+		}{
+			{
+				"A,B,C,D,E",
+				",",
+			}, {
+				"1.2.3.4",
+				".",
+			}, {
+				"C",
+				",",
+			},
+		}
+
+		for _, test := range tests {
+			testRun(test.Value, test.delimiter)
+		}
+	}()
+}
