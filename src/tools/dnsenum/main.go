04f106f9d2192ce685b3b9306ca477dfc29080e2
Sam Caldwell <github@samcaldwell.net>
2020-11-08 11:54:22 -0600

    implement buildConfig in dnsEnum.

--- src/tools/dnsenum/main.go
+++ src/tools/dnsenum/main.go
@@ -6,34 +6,59 @@ package main
 */
 
 import (
+	"asymmetric-effort/asymmetric-toolkit/buildConfig"
 	"asymmetric-effort/asymmetric-toolkit/src/common/cli"
 	"asymmetric-effort/asymmetric-toolkit/src/common/logger"
 	"asymmetric-effort/asymmetric-toolkit/src/common/source"
-	"asymmetric-effort/asymmetric-toolkit/src/tools/dnsenum/deprecated_cli"
 	"fmt"
 	"os"
 )
 
 const (
+	programName    = "dnsName"
 	dnsRecordTypes = "A,AAAA,MX,TXT,SOA,NS,CNAME"
+	sourceBufferSz = 1048576
 )
+
+/*
+const (
+	Version               string                = "0.0.1"
+	defaultConcurrency    types.PositiveInteger = 1  //default number of concurrent queries to run.
+	defaultDepth          types.PositiveInteger = 1  //default number of DNS subdomain levels to attack
+	maxDepth              types.PositiveInteger = 20 //Maximum number of DNS subdomain levels to attack
+	defaultTimeout        types.PositiveInteger = 60 //Default number of seconds before connection timeout.
+	defaultFilterPattern  string                = `.+`
+	defaultDnsRecordTypes string                = "A,AAAA,MX,CNAME,NS,TXT,SOA,SRV"
+	defaultWordSize       types.PositiveInteger = 5 //For a given sequence or random string, this is the max length of the 'word'
+	DnsChars              string                = "WMEFSABCDGHIJKLNOPQRTUVXYZwmefsabcdghijklnopqrtuvxyz0123456789._-"
+	SourceBufferSz        int                   = 1048576 //Size of the source buffer which feeds the payload (attack) function.
+	ResponseBufferSz      int                   = 1048576 //Size of the response buffer for responses pending report processing.
+
+	expectFlag  int = 0
+	expectValue int = 1
+
+	ExitTerminate bool = true
+	ExitParseOk   bool = false
+)
+
+*/
 
 func main() {
 	//
 	// Setup our program to run
 	//
 	var exit chan int = make(chan int, 1) // We will block until an exit code is written to this channel.
-	var config *cli.ArgumentMap			  // Runtime program configuration.
-	var log logger.Logger				  // Global logger
-	var feed source.Source				  // Source Generator
+	var config *cli.ArgumentMap           // Runtime program configuration.
+	var log logger.Logger                 // Global logger
+	var feed source.Source                // Source Generator
 	//var attack dnsEnumerator			  // Attacker Payload
 	func() {
 		var spec cli.Specification = cli.Specification{
-			Author:      "Sam Caldwell",
-			AuthorEmail: "mail@samcaldwell.net",
-			ProgramName: "Asymmetric Effort",
-			Copyright:   "(c) 2018 Sam Caldwell.  All Rights Reserved.",
-			Version:     "0.0.1",
+			Author:      buildconfig.Author,
+			AuthorEmail: buildconfig.AuthorEmail,
+			ProgramName: programName,
+			Copyright:   buildconfig.Copyright,
+			Version:     buildconfig.Version,
 			Argument: map[string]cli.ArgumentDescriptor{
 				"recordTypes": {
 					1000,
@@ -58,14 +83,14 @@ func main() {
 				//
 				// ToDo: get properties out of ui and free that memory.
 				//
-				config=&ui.Arguments
+				config = &ui.Arguments
 			}
 		}
 	}()
 	log.Setup(&ui)
 	log.Debug("Main(): Logger is setup and config is loaded.")
 
-	feed.Setup(&ui, deprecated_cli.SourceBufferSz, deprecated_cli.DnsChars)
+	//feed.Setup(&ui, deprecated_cli.SourceBufferSz, deprecated_cli.DnsChars)
 
 	//var requestSent chan bool = make(chan bool, 1)
 
