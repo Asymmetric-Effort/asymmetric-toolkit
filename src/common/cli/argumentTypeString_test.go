282329a1e2deaf91324f336e47970877103aaafb
Sam Caldwell <github@samcaldwell.net>
2020-11-08 10:07:30 -0600

    Debugging argumentTypeString_test

--- src/common/cli/argumentTypeString_test.go
+++ src/common/cli/argumentTypeString_test.go
@@ -48,40 +48,26 @@ func TestArgumentType_String(t *testing.T) {
 	recoverErrorFunc := noop
 
 	fmt.Println("---")
-
 	testRun := func(pass bool, token ArgumentType, expected string) {
 
 		fmt.Printf("Testing... (%d): '%s' (%v)\n", token, expected, pass)
-
 		if pass {
-
 			fmt.Printf("Expect pass\n")
-
 			recoverErrorFunc = noop
-
 		} else {
-
 			fmt.Println("Expect failure")
-
 			recoverErrorFunc = func() {
-
 				fmt.Println("Error occurred but was recovered")
-
 				recover()
-
 			}
-
 		}
+
 		defer recoverErrorFunc()
 
 		fmt.Printf("Preparing to call .String() method for %d\n",token)
-
 		if token.String() == expected {
-
 			fmt.Println("We're ok")
-
 			return
-
 		} else {
 			panic(fmt.Sprintf("Token(%d) failed: %s", token, expected))
 		}
