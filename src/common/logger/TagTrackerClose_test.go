1644bee908532a638e99e105c432550bc963abf5
Sam Caldwell <github@samcaldwell.net>
2020-11-14 22:44:47 -0600

    adding tests for common/logger TagTracker

--- src/common/logger/TagTrackerClose_test.go
+++ src/common/logger/TagTrackerClose_test.go
@@ -1,2 +1,52 @@
 package logger
 
+import (
+	"asymmetric-effort/asymmetric-toolkit/src/common/errors"
+	"testing"
+)
+
+func TestTagTracker_Close(t *testing.T) {
+	var o TagTracker
+	var tagId TagId
+	var nextTagId TagId
+
+	func() { // Setup
+		tagId = o.Create("test0")
+		errors.Assert(tagId == 0, "expected TagId 0")
+		errors.Assert(int(o.tagNames["test0"]) == 0, "expect tagNames")
+		errors.Assert(o.tagIds[0] == "test0", "expect tagIds")
+	}()
+
+	func() {
+		nextTagId = o.Create("test1")
+		errors.Assert(nextTagId == 1, "expected TagId 1")
+	}()
+
+	func() { // Test
+		o.Close(tagId) //Close tagId 0
+		defer func() { recover() }()
+		errors.Assert(tagId == 0, "expected TagId 0")
+		errors.Assert(int(o.tagNames["test0"]) == 0, "expect error because this should be deleted.")
+		errors.Assert(o.tagIds[0] == "test0", "expect error because this should be deleted.")
+	}()
+
+	func() { //Test second delete
+		defer func() { recover() }()
+		o.Close(tagId)
+		panic("Test second delete expected error.")
+	}()
+
+	func() { // Make 2nd tag global
+		o.AddGlobal(nextTagId)
+		errors.Assert(o.global[nextTagId] == struct{}{}, "Expect 2nd tag to be global.")
+	}()
+
+	func() { // Test
+		o.Close(nextTagId) //Close tagId 1
+		defer func() { recover() }()
+		errors.Assert(nextTagId == 1, "expected TagId 1")
+		errors.Assert(o.tagNames["test1"] == nextTagId, "expect error because this should be deleted.")
+		errors.Assert(o.tagIds[nextTagId] == "test1", "expect error because this should be deleted.")
+		errors.Assert(o.global[nextTagId] == struct{}{}, "expect error.")
+	}()
+}
--- src/common/logger/TagTrackerCreate_test.go
+++ src/common/logger/TagTrackerCreate_test.go
@@ -1,7 +1,232 @@
 package logger
 
-import "testing"
+import (
+	assert "github.com/sam-caldwell/adrestia-assertions/src"
+	"testing"
+)
 
-func TestLogger_TagTrackerCreate(t *testing.T){
-
+func TestLogger_TagTrackerCreate(t *testing.T) {
+	func() {
+		var o TagTracker
+		tagId := o.Create("test0")
+		assert.Error(tagId == 0, "expected TagId 0")
+		tagId = o.Create("test1")
+		assert.Error(tagId == 1, "expected TagId 1")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test 1")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("testbadspace01234012345678901234567898567890132365498765432132165465464")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test-1")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test_1")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test+1")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("_")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test!")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test@")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test#")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test$")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test%")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test^")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test&")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test*")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test$")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test(")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test)")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test${")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test}")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test[")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test]")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test:")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test;")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test'")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test/")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test?")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test>")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test<")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test.")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test,")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test|")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test\\")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test=")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test~")
+		panic("Expected error on bad tag.")
+	}()
+	func() {
+		var o TagTracker
+		defer func() { recover() }()
+		_ = o.Create("test`")
+		panic("Expected error on bad tag.")
+	}()
 }
