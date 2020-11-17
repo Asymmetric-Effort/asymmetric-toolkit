6e1c362ffb49c0d9f1da1147ec94096db1e81687
Sam Caldwell <github@samcaldwell.net>
2020-11-17 11:26:58 -0600

    remove dictionaryTool in favor of src/tools/dictionary/*

--- src/tools/dictionaryTool/programDescription.go
+++
@@ -1,7 +0,0 @@
-package main
-
-const (
-	Description = `This program is used to create, manage, edit and maintain dictionary files which are formatted 
-using encryption, ranking and other metadata to improve performance of the dictionary attacks possible through the
-asymmetric toolkit.`
-)
