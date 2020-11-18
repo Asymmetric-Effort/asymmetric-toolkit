02dc4821ff0d804dfb3ea0fe9042669a56808f93
Sam Caldwell <github@samcaldwell.net>
2020-11-17 20:00:11 -0600

    WIP: resuming work on common/source

--- src/common/source/generatedictionary.go
+++ src/common/source/generatedictionary.go
@@ -1,18 +1,17 @@
 package source
 
-import (
-	"asymmetric-effort/asymmetric-toolkit/src/common/errors"
-)
-
 func (o *Source) generateDictionary() {
 	/*
 		Read the o.dict file (observe pauseFlag)
 	*/
-	errors.Assert(o.config != nil,"Source::generateDictionary() encountered nil config")
-	/*var dict reader.Reader
-	defer dict.Close()
-	scanner:=dict.Setup(o.config.Dictionary.OpenRead())
-	for line:=scanner(); line != nil; line=scanner(){
+	/*
+	errors.Assert(o.config != nil, "Source::generateDictionary() encountered nil config")
+	var dictionary DictionaryReader.Reader
+	defer dictionary.Close()
+	dictionary.Setup(dictionary.Read, o.config) //Configure the dictionary.
+
+	dictionary.OpenRead()
+	for line := scanner(); line != nil; line = scanner() {
 		o.WaitIfPaused()
 		o.queue.Push(*line)
 		o.counter++
