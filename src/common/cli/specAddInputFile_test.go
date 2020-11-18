90416b4ae7f90a7cf0c8c331994b5719bae3cade
Sam Caldwell <github@samcaldwell.net>
2020-11-17 21:19:36 -0600

    add tests for common/cli generic input/output file specs

--- src/common/cli/specAddInputFile_test.go
+++ src/common/cli/specAddInputFile_test.go
@@ -1,7 +1,21 @@
 package cli
 
-import "testing"
+import (
+	"asymmetric-effort/asymmetric-toolkit/src/common/errors"
+	"fmt"
+	"testing"
+)
 
 func TestCommandline_AddInputFile(t *testing.T){
-	t.SkipNow()
+	var o Specification
+	o.AddInputFile()
+	errors.Assert(o.Argument != nil, "Expected nil ArgumentDescriptor")
+	errors.Assert(o.Argument[inFileArgLong].FlagId == FlagInputFile,
+		fmt.Sprintf("(%s) expected (%d)  FlagId:%d",
+			inFileArgLong, FlagInputFile, o.Argument[inFileArgLong].FlagId))
+	errors.Assert(o.Argument[inFileArgLong].Type == String, "Expected string type")
+	errors.Assert(o.Argument[inFileArgLong].Expects == ExpectValue, "Expects a value")
+	errors.Assert(o.Argument[inFileArgLong].Default == "", "Expect empty string")
+	errors.Assert(o.Argument[inFileArgLong].Help == inFileHelpText, "expect help text.")
+	errors.Assert(o.Argument[inFileArgLong].Parse != nil, "expect non nil parse function.")
 }
--- src/common/cli/specAddOutputFile_test.go
+++ src/common/cli/specAddOutputFile_test.go
@@ -1,7 +1,21 @@
 package cli
 
-import "testing"
+import (
+	"asymmetric-effort/asymmetric-toolkit/src/common/errors"
+	"fmt"
+	"testing"
+)
 
 func TestCommandLine_AddOutputFile(t *testing.T) {
-	t.SkipNow()
+	var o Specification
+	o.AddOutputFile()
+	errors.Assert(o.Argument != nil, "Expected nil ArgumentDescriptor")
+	errors.Assert(o.Argument[outFileArgLong].FlagId == FlagOutputFile,
+		fmt.Sprintf("(%s) expected (%d)  FlagId:%d",
+			outFileArgLong, FlagOutputFile, o.Argument[outFileArgLong].FlagId))
+	errors.Assert(o.Argument[outFileArgLong].Type == String, "Expected string type")
+	errors.Assert(o.Argument[outFileArgLong].Expects == ExpectValue, "Expects a value")
+	errors.Assert(o.Argument[outFileArgLong].Default == "", "Expect empty string")
+	errors.Assert(o.Argument[outFileArgLong].Help == outFileHelpText, "expect help text.")
+	errors.Assert(o.Argument[outFileArgLong].Parse != nil, "expect non nil parse function.")
 }
