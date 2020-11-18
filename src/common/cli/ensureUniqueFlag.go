7e0bffc654ea8cef43c8b29a75e878436780ed5f
Sam Caldwell <github@samcaldwell.net>
2020-11-17 22:55:51 -0600

    fix error message in ensureUniqueFlag

--- src/common/cli/ensureUniqueFlag.go
+++ src/common/cli/ensureUniqueFlag.go
@@ -9,13 +9,21 @@ import "fmt"
 */
 
 func (o *Specification) EnsureUniqueFlagId() (err error) {
+
 	seenBefore := make(map[ArgumentFlag]bool)
+
 	for name, arg := range o.Argument {
+
 		ok := seenBefore[arg.FlagId]
+
 		if ok {
-			return fmt.Errorf("duplicate ArgumentDescriptor flagId in Specification (%s)[%d]", name, arg.FlagId)
+			return fmt.Errorf("duplicate flagId (%d) in Specification (%s)", arg.FlagId, name)
 		}
+
 		seenBefore[arg.FlagId] = true
+
 	}
+
 	return nil
+
 }
